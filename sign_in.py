4ff5351bca6a1c2aeb94cd8412d128177c12f3d7
CruserShadow <cruser_shadow@mail.ru>
2021-03-22 07:58:06 +1000

    some upgrades in code

--- sign_in.py
+++ sign_in.py
@@ -1,11 +1,10 @@
 from exception_vvsu import VvsuException, IncorrectDataException
 
 import requests
+
 from requests import HTTPError
-import re
 from lxml import etree
 from lxml.etree import Element
-import time
 
 
 class Vvsu:
@@ -15,29 +14,21 @@ class Vvsu:
                           "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0",
                           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
                           "Referer": "https://cabinet.vvsu.ru/",
-                          "Accept-Encoding": "gzip, deflate, br"}
+                          "Accept-Encoding": "gzip, br"}
 
         self.__session = requests.Session()
+        self.__session.headers = self.__HEADERS
+
         self.URL = "https://cabinet.vvsu.ru/"
 
         self.__logged = False
         self.__name = None
         self.__surname = None
 
-        for key in self.__HEADERS.keys():
-            self.__change_session_headers(key, self.__HEADERS[key])
-
-    def __change_session_headers(self, key, value):
-        self.__session.headers[key] = value
-
-    def _set_name(self, name):
-        self.__name = name
-
-    def _set_surname(self, surname):
-        self.__surname = surname
-
-    def _post_data(self, usr, passwd):
-        return self.__session.post(self.URL, data={"user": usr, "pass": passwd})
+    def _post_data(self, link, **kwargs):
+        for k in list(kwargs.keys()):
+            kwargs[str(k).lstrip("_")] = kwargs.pop(k)
+        return self.__session.post(link, data=kwargs)
 
     def _parse_fio(self, document):
         doc: Element = etree.HTML(document)
@@ -45,36 +36,35 @@ class Vvsu:
 
         if elem:
             fio = list(elem[0])[0].text.split()
-
-            self._set_name(fio[1])
-            self._set_surname(fio[0])
+            self.__name = fio[1]
+            self.__surname = fio[0]
         else:
             raise IncorrectDataException("Incorrect username or password")
 
     def login_into_vvsu(self, username, password):
-        response = self._post_data(username, password)
-        print(response.request.body)
+        response = self._post_data(self.URL, user=username, _pass=password)
         response.raise_for_status()
 
         body = response.content.decode(response.apparent_encoding)
         try:
             self._parse_fio(body)
-            self.__logged = True
-            print("Logged in")
         except IncorrectDataException as err:
             print(err)
+        else:
+            self.__logged = True
+            print("Logged in")
 
     def sign_out_vvsu(self):
         if self.__logged:
             try:
                 response = self.__session.get(f"{self.URL}?logout=1")
+
                 response.raise_for_status()
             except HTTPError as err:
                 print(err, type(err))
 
             else:
-                really_quit = re.search("Войти в систему", response.content.decode(response.apparent_encoding))
-                if really_quit:
+                if "Войти в систему" in response.content.decode(response.apparent_encoding):
                     self.__session.close()
                     print("Logged out")
         else:
